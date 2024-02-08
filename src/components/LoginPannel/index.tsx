88f0b79817ca965d39dbf5d651adba05fedc7950
edmilsonmedeiross <edmilson_jb@hotmail.com>
2024-02-07 23:42:54 -0300

    feat/ -> finaliza tela de login simples

--- src/components/LoginPannel/index.tsx
+++ src/components/LoginPannel/index.tsx
@@ -15,18 +15,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { setUserToLocalStorage, setUsersToLocalStorage } from "@/lib/utils";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username precisa ter no mínimo 2 caracteres.",
-  }),
-  email: z.string().email({
-    message: "Email inválido.",
-  }),
-});
+import { useRouter, usePathname } from "next/navigation";
+import { formSchema } from "./validation";
+import { useFormSubmit } from "@/hooks/useFormSubmit";
 
 function LoginPannel() {
+  const router = useRouter();
+  const pathName = usePathname();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,51 +30,64 @@ function LoginPannel() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-    setUserToLocalStorage(values);
-    setUsersToLocalStorage(values);
-  }
+  const { setError } = form;
+  const { onSubmit } = useFormSubmit({
+    formSchema,
+    router,
+    setError,
+    pathName,
+  });
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-1/2">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="username" {...field} />
-              </FormControl>
-              <FormDescription>
-                Este será o nome mostrado no seu perfil.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="email" {...field} />
-              </FormControl>
-              <FormDescription>
-                Este email será usado para entrar na sua conta.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Entrar</Button>
-      </form>
+      <div className="flex flex-col w-full items-center h-screen justify-evenly">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Login
+        </h1>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-1/2"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="username" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Username utilizado no seu cadastro.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="email" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Email utilizado no seu cadastro.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex gap-2">
+            <Button type="submit">Entrar</Button>
+            <Button type="button" onClick={() => router.push("/register")}>
+              Cadastrar
+            </Button>
+          </div>
+        </form>
+      </div>
     </Form>
   );
 }
