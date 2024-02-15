57cc0a637290e7c88276f444c9b9fbe9e327fc1c
edmilsonmedeiross <edmilson_jb@hotmail.com>
2024-02-15 02:55:16 -0300

    refactor/ -> refatora model

--- src/components/ConfirmationModal/index.tsx
+++ src/components/ConfirmationModal/index.tsx
@@ -1,36 +1,94 @@
+"use client";
 import { useRouter } from "next/navigation";
 import {
   Card,
   CardContent,
   CardDescription,
   CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-function ConfirmationModal({ isOpen, onClose, room }) {
+type ConfirmationModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  room: {
+    id: string;
+    name: string;
+    isPrivate: "private" | "public";
+    password?: string;
+  };
+};
+
+function ConfirmationModal({ isOpen, onClose, room }: ConfirmationModalProps) {
   const router = useRouter();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const password = e.target.password.value;
 
-    console.log(password);
+  const FormSchema = z.object({
+    password: z.string().min(3, "Senha precisa ter no mínimo 3 caracteres."),
+  });
 
-    onClose();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(
+      FormSchema.refine(
+        (data) => {
+          if (room.isPrivate === "private") {
+            return data.password === room.password;
+          }
+          return true;
+        },
+        { message: "Senha incorreta", path: ["password"] }
+      )
+    ),
+  });
 
-    router.push(`/${room.id}`);
+  const { watch, reset } = form;
+  const onSubmit = () => {
+    const password = watch("password");
+
+    if (password === room.password) {
+      router.push(`/room/${room.id}`);
+      reset();
+      onClose();
+    }
   };
 
   return isOpen ? (
-    <Card>
+    <Card className="absolute bottom-1/3 left-1/4 p-2 max-md:w-full max-md:top-auto max-md:left-auto max-md:mx-4">
       <CardContent>
-        <CardDescription>
+        <CardDescription className="m-3">
           Essa sala é privada, por favor, insira a senha para acessar.
         </CardDescription>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="password">Senha:</label>
-          <input id="password" name="password" type="password" />
-          <button type="submit">Entrar</button>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-2"
+        >
+          <Form {...form}>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Entrar</Button>
+          </Form>
         </form>
       </CardContent>
       <CardFooter>
